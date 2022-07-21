e5d4bb5e24048b12c3b755e87a1b3d6ca25d9475
adnKsharp <darkmark404@protonmail.com>
2022-07-20 23:26:19 -0600

    First...

---
+++ CustomBorder/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CustomBorder
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkRed;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(23, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(41, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(23, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.ForeColor = System.Drawing.Color.White;
+			this.button3.Location = new System.Drawing.Point(70, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(23, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button4.FlatAppearance.BorderSize = 0;
+			this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.ForeColor = System.Drawing.Color.White;
+			this.button4.Location = new System.Drawing.Point(99, 12);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(23, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button5.FlatAppearance.BorderSize = 0;
+			this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSlateGray;
+			this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(201)))), ((int)(((byte)(32)))));
+			this.button5.Location = new System.Drawing.Point(128, 12);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(340, 23);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			this.button5.MouseEnter += new System.EventHandler(this.button5_MouseEnter);
+			this.button5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button5_MouseMove);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Black;
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.ForeColor = System.Drawing.Color.White;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1920, 48);
+			this.panel1.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(39)))), ((int)(((byte)(47)))));
+			this.ClientSize = new System.Drawing.Size(480, 240);
+			this.Controls.Add(this.panel1);
+			this.Font = new System.Drawing.Font("SF Pro Rounded", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ CustomBorder/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomBorder
+{
+	public partial class Form1 : Form
+	{
+		bool max = false, min = false, pos = false;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button1.Text = "X";
+			button2.Text = "O";
+			button3.Text = "-";
+			button4.Text = "V";
+			button5.Text = "Form1";
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			max = !max;
+			if (max)
+				WindowState = FormWindowState.Maximized;
+			else
+				WindowState = FormWindowState.Normal;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			WindowState = FormWindowState.Minimized;
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			min = !min;
+			if (min) 
+				Size = new Size(480, 48);
+			else
+				Size = new Size(480, 240);
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			pos = !pos;
+		}
+
+		private void button5_MouseEnter(object sender, EventArgs e)
+		{
+			if (pos) locate();
+		}
+
+		private void button5_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (pos) locate();
+		}
+
+		public void locate()
+		{
+			Location = new Point(int.Parse(Cursor.Position.X.ToString()) - 300, int.Parse(Cursor.Position.Y.ToString()) - 25);
+		}
+	}
+}
